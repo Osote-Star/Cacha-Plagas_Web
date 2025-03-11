ab28cdae4f17f3aaffa411bc33d5268c5470c7de
Osote-Star <5321100090@correcaminos.uts.edu.mx>
2025-03-11 16:53:01 -0700

    Componente validar codigo añadido

---
+++ src/app/Component/validar-codigo/validar-codigo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validar-codigo',
+  imports: [],
+  templateUrl: './validar-codigo.component.html',
+  styleUrl: './validar-codigo.component.css'
+})
+export class ValidarCodigoComponent {
+
+}
