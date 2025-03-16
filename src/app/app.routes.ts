d0fb4a979da0444aee94242c9398cfc1f6937549
Martin3316 <5323100289@correcaminos.uts.edu.mx>
2025-03-16 14:37:42 -0700

    Estadisticas version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Component/login/login.component';
+import { EstadisticasComponent } from './Component/estadisticas/estadisticas.component';
 
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent }
+    { path: '', component: EstadisticasComponent },
 ];
