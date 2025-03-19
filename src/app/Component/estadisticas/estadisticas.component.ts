6111a596cf397d7790acbce5290921aaee5b44b2
Martin3316 <5323100289@correcaminos.uts.edu.mx>
2025-03-19 14:17:36 -0700

    Estadisticas version 4

--- src/app/Component/estadisticas/estadisticas.component.ts
+++ src/app/Component/estadisticas/estadisticas.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { GraficaComponent } from './grafica/grafica.component';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [GraficaComponent],
+  imports: [GraficaComponent, CommonModule],
   templateUrl: './estadisticas.component.html',
-  styleUrl: './estadisticas.component.css',
+  styleUrls: ['./estadisticas.component.css'], // Corregido
   standalone: true
 })
 export class EstadisticasComponent {
+  esAdmin: boolean = false; // Variable para identificar si es admin o usuario
 
+  constructor() {
+    // Verifica si estamos en un entorno de navegador antes de acceder a localStorage
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const rol = localStorage.getItem('rol'); // Supongamos que el rol se guarda en localStorage
+      if (rol === 'admin') {
+        this.esAdmin = false; // Comprobamos si es admin
+      } else {
+        this.esAdmin = true; // Asumimos que es usuario si no es admin
+      }
+    }
+  }
 }
