47771501c0bd1e7324b88c8a4236741b5edd0292
Martin3316 <5323100289@correcaminos.uts.edu.mx>
2025-03-18 21:55:56 -0700

    Estadisticas version 2

--- src/app/Component/estadisticas/estadisticas.component.ts
+++ src/app/Component/estadisticas/estadisticas.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { GraficaComponent } from './grafica/grafica.component';
+
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [],
+  imports: [GraficaComponent],
   templateUrl: './estadisticas.component.html',
-  styleUrl: './estadisticas.component.css'
+  styleUrl: './estadisticas.component.css',
+  standalone: true
 })
 export class EstadisticasComponent {
 
---
+++ src/app/Component/estadisticas/grafica/grafica.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+@Component({
+  selector: 'app-grafica',
+  standalone: true,
+  templateUrl: './grafica.component.html',
+  styleUrls: ['./grafica.component.css']
+})
+export class GraficaComponent {
+
+  public config: any = {
+    type: 'bar',
+    data: {
+      labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Agto', 'Sept', 'Oct', 'Nov', 'Dic'],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [2, 3, 4, 4, 7, 3, 9, 3, 1, 11, 7, 3],
+        backgroundColor: [
+          'rgb(187, 33, 79)', 'rgb(138, 17, 108)', 'rgb(74, 5, 92)', 'rgb(8, 51, 129)', 
+          'rgb(20, 192, 155)', 'rgb(19, 138, 35)', 'rgb(83, 226, 47)', 'rgb(245, 255, 105)', 
+          'rgb(247, 215, 37)', 'rgb(240, 161, 42)', 'rgb(202, 102, 9)', 'rgb(194, 16, 16)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true,  // Hacer que el gráfico se ajuste al tamaño del contenedor
+      maintainAspectRatio: false,  // Permite que el gráfico ocupe el tamaño completo disponible
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  chart: any;
+
+  ngOnInit(): void {
+    this.chart = new Chart('chart', this.config);
+  }
+}
