f0443087766720d46112671e8b6bc7f699abf2c8
Martin3316 <5323100289@correcaminos.uts.edu.mx>
2025-03-19 08:12:59 -0700

    estadisticas version 3

--- src/app/Component/estadisticas/grafica/grafica.component.ts
+++ src/app/Component/estadisticas/grafica/grafica.component.ts
@@ -13,10 +13,10 @@ export class GraficaComponent {
   public config: any = {
     type: 'bar',
     data: {
-      labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Agto', 'Sept', 'Oct', 'Nov', 'Dic'],
+      labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
       datasets: [{
-        label: 'My First Dataset',
-        data: [2, 3, 4, 4, 7, 3, 9, 3, 1, 11, 7, 3],
+        label: 'Capturas',
+        data: [2, 3, 4, 4, 7, 3, 9 ],
         backgroundColor: [
           'rgb(187, 33, 79)', 'rgb(138, 17, 108)', 'rgb(74, 5, 92)', 'rgb(8, 51, 129)', 
           'rgb(20, 192, 155)', 'rgb(19, 138, 35)', 'rgb(83, 226, 47)', 'rgb(245, 255, 105)', 
