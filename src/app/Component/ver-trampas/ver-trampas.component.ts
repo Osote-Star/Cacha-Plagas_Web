08e763cea45aedcb1f1e1a2aa18fbc246f8dd5d2
AngelAstorga08 <angeledastorga08@gmail.com>
2025-03-20 08:03:16 -0700

    Agregacion de el componente verTrampa y los servicios de usuario y trampa

--- src/app/Component/ver-trampas/ver-trampas.component.ts
+++ src/app/Component/ver-trampas/ver-trampas.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ver-trampas',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './ver-trampas.component.html',
   styleUrl: './ver-trampas.component.css'
 })
 export class VerTrampasComponent {
-
+  products = Array(8).fill({
+    id: '9347',
+    name: 'RAT-TRAP',
+    description: 'Especializada en ratas, esta trampa es simple y efectiva, cumple su objetivo gracias a su tamaño y materiales resistentes.',
+    imageUrl: '/public/rat-trap.png'
+  });
 }
